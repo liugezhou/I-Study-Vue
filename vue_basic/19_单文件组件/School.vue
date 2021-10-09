defbf2b489908b19e6d539f2b0daccd312d0c3b0
liumingzhou <18231133236@163.com>
2021-10-09 17:00:14 +0800

    单文件组件

---
+++ vue_basic/19_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="demo">
+		<h2>名字：{{name}}</h2>
+		<h2>年龄：{{age}}</h2>
+		<button @click="showName">点我提示我的名</button>	
+	</div>
+</template>
+
+<script>
+	 export default {
+		name:'School',
+		data(){
+			return {
+				name:'LiuGeZhou',
+				age:'18'
+			}
+		},
+		methods: {
+			showName(){
+				alert(this.name)
+			}
+		},
+	}
+</script>
+
+<style>
+	.demo{
+		background-color: orange;
+	}
+</style>

d8aef5bc0e571a3be205751e650f8114453961b7
liumingzhou <18231133236@163.com>
2021-11-09 16:35:04 +0800

    add:Vue3_base

---
+++ vue3_test/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- Vue3组件中模版结构可以没有根标签 -->
+  <div>
+    <h1>setup感受</h1>
+    <h2>name:{{ name }}</h2>
+    <h3>age:{{ age }}</h3>
+    <button @click="say">点我</button>
+  </div>
+</template>
+
+<script>
+import { h } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let name = "Tom";
+    let age = "18";
+
+    function say() {
+      console.log(`${name} 今年 ${age}了！`);
+    }
+    return {
+      name,
+      age,
+      say,
+    };
+  },
+};
+</script>
+
+<style>
+</style>

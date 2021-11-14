5b29ae76c59ec4f590ef25f146034264f1f87c28
liumingzhou <18231133236@163.com>
2021-11-14 14:26:52 +0800

    生命周期

---
+++ vue3_test/09_Vue3生命周期/components/Demo.vue
@@ -0,0 +1,78 @@
+<template>
+  <!-- Vue3组件中模版结构可以没有根标签 -->
+  <div>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="sum++">+1</button>
+  </div>
+</template>
+
+<script>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+export default {
+  name: "Demo",
+  setup() {
+    //数据
+    let sum = ref(0);
+    console.log('setup')
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("onMounted");
+    });
+    onBeforeUpdate(() => {
+      console.log("onBeforeUpdate");
+    });
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+    onBeforeUnmount(() => {
+      console.log("onBeforeUnmount");
+    });
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    });
+    //通过组合api的形式去使用生命周期钩子
+    return {
+      sum,
+    };
+  },
+  //#region
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+  //#endregion
+};
+</script>
+
+<style>
+</style>

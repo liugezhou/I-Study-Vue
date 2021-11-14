dad16d72431f7acf9370651855e02d1d91e01172
liumingzhou <18231133236@163.com>
2021-11-14 18:17:12 +0800

    toRaw&markRaw

---
+++ vue3_test/14_toRaw&markRaw/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- Vue3组件中模版结构可以没有根标签 -->
+  <div>
+    <button @click="isShowDemo =!isShowDemo">切换</button>
+    <Demo v-if="isShowDemo"/>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import Demo from "./components/Demo.vue";
+export default {
+  name: "App",
+  components: {
+    Demo,
+  },
+  setup() {
+    let isShowDemo = ref(true)
+    return {
+      isShowDemo
+    }
+  }
+};
+</script>
+
+<style>
+</style>

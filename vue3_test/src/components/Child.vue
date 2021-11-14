749b63ea50ad4f70ceab0713c6538bce8516376b
liumingzhou <18231133236@163.com>
2021-11-14 21:21:23 +0800

    teleport

---
+++ vue3_test/src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Vue3组件中模版结构可以没有根标签 -->
+  <div class="child">
+    <h3>我是Child组件</h3>
+    <Son/>
+  </div>
+</template>
+
+<script>
+import Son from './Son'
+export default {
+  name: "Child",
+  components:{
+    Son
+  },
+};
+</script>
+
+<style>
+.child{
+  background-color:skyblue;
+  padding:10px;
+}
+</style>
---
+++ vue3_test/src/components/Son.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Vue3组件中模版结构可以没有根标签 -->
+  <div class="son">
+    <h3>我是Son组件</h3>
+    <Dialog/>
+  </div>
+</template>
+
+<script>
+import Dialog from './Dialog.vue'
+export default {
+  name: "Son",
+  components:{
+    Dialog
+  }
+};
+</script>
+
+<style>
+.son {
+  background-color: yellowgreen;
+  padding: 10px;
+}
+</style>
